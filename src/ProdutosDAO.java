5c9918b68966b9b78d4737111a40b6b3acce5c2b
Ikarol2 <vitoriia3110@gmail.com>
2025-04-16 14:58:30 -0300

    Operação adicionada, listarProdutosVendidos()

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.sql.SQLException;
+import java.util.List;
 
 public class ProdutosDAO {
 
@@ -64,5 +65,46 @@ public class ProdutosDAO {
 
         return listagem;
    }
+    public List<ProdutosDTO> listarProdutosVendidos(){
+
+        try {
+            conn = new conectaDAO().connectDB();
+            
+            String sql;
+            sql = "SELECT * FROM uc11.produtos WHERE status = 'Vendido';";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery(sql);
+            
+            List<ProdutosDTO> lista_vendido = new ArrayList<>();
+            
+            while(resultset.next()){
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+                produtosDTO.setId(resultset.getInt(1));
+                produtosDTO.setNome(resultset.getString(2));
+                produtosDTO.setValor(resultset.getInt(3));
+                produtosDTO.setStatus(resultset.getString(4));
+
+                lista_vendido.add(produtosDTO);
+            }
+            return lista_vendido;
+        } catch (SQLException ex) {
+            return null;
+        }finally {
+            
+            try {
+                if (resultset != null) {
+                    resultset.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+    }    
+    }
 
 }
