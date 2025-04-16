78fb43eaf366bb1c03f5bbf54e18c3e2a9c635dd
Ikarol2 <vitoriia3110@gmail.com>
2025-04-16 00:18:11 -0300

    Mensagem para informar se o produto foi registrado ou não

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,62 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
 
+    
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        try {
+            conn = new conectaDAO().connectDB();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Salvo com sucesso");
+          
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Erro ao salvar");
+
+        }
+    }
+    
+    /*public ArrayList<ProdutosDTO> listarProdutos() {
+         ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        String sql = "SELECT id, nome, valor, status FROM produtos";
+        
+        try {
+            conn = new conectaDAO().connectDB(); // Estabelece a conexão
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery();
+            
+            while (resultSet.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultSet.getInt("id"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setValor(resultSet.getInt("valor"));
+                produto.setStatus(resultSet.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        
+
+        return listagem;
+   }*/
+
+}
